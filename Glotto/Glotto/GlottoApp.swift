4262e5f0ccf9c13a5f2570b592381415e14341b9
mattyh867 <matthewhughes.867@gmail.com>
2026-08-18 15:00:25 +0100

    Created Xcode project

---
+++ Glotto/Glotto/GlottoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlottoApp.swift
+//  Glotto
+//
+//  Created by Matthew Hughes on 18/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct GlottoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
